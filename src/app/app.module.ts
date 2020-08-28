8660f2803f2056d32c41743f9ae25d1f89bdd3ae
Tiet Le Bao Khanh <khanh.tiet.6001@gmail.com>
2020-08-28 09:59:35 +0700

    remove import 2 dynamic modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ExampleContainerComponent } from "./example-container/example-container.component";
-import { DynamicContentOneComponent } from "./dynamic-content-one/dynamic-content-one.component";
-import { DynamicContentTwoComponent } from "./dynamic-content-two/dynamic-content-two.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ExampleContainerComponent,
-    DynamicContentOneComponent,
-    DynamicContentTwoComponent,
   ],
   imports: [BrowserModule],
   providers: [],
